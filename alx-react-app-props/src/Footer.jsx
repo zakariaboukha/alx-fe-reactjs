58e11315fb913191564dfea01486a6939f111539
zakariaboukha <zakariaboy99@gmail.com>
2024-11-11 00:34:48 +0100

    Create Footer.jsx

---
+++ alx-react-app-props/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px' }}>
+          <p>© 2023 City Lovers</p>
+      </footer>
+  );
+}
+
+export default Footer;
