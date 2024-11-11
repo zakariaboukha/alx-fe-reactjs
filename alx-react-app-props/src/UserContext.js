5c9d72982be72aaad6dba09b81943227128b80ca
zakariaboukha <zakariaboy99@gmail.com>
2024-11-11 01:02:58 +0100

    Update UserContext.js

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,14 +1,6 @@
-// src/UserContext.js
-import React, { createContext } from 'react';
+// UserContext.js
+import { createContext } from 'react';
 
-// Step 1: Create a context for the user data
-export const UserContext = createContext();
+const UserContext = createContext(null);
 
-// Step 2: Create a provider component
-export const UserProvider = ({ children, userData }) => {
-  return (
-    <UserContext.Provider value={userData}>
-      {children}
-    </UserContext.Provider>
-  );
-};
+export default UserContext;
