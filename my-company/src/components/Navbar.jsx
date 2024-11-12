04ec352794ff74939e2ae22cf5fea135a67a2b1e
zakariaboukha <zakariaboy99@gmail.com>
2024-11-12 04:56:56 +0100

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-// src/Navbar.jsx
+// src/components/Navbar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const navbarStyle = {
+    backgroundColor: '#ccc', // Add background color
+    display: 'flex', // Use flexbox for layout
+    justifyContent: 'space-around', // Adjust the space around the links
+    padding: '10px'
+  };
+
   return (
-    <nav style={{ padding: '10px', background: '#ccc' }}>
+    <nav style={navbarStyle}>
       <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
       <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
@@ -14,3 +21,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
