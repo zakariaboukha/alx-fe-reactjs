2909aba8786e76f5135e907f6db2750f672223c9
zakariaboukha <zakariaboy99@gmail.com>
2024-11-17 18:16:05 +0100

    Create RecipeDetails.jsx

---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useRecipeStore } from '../recipeStore';
+import EditRecipeForm from './EditRecipeForm';
+import DeleteRecipeButton from './DeleteRecipeButton';
+
+const RecipeDetails = ({ recipeId }) => {
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((recipe) => recipe.id === recipeId)
+  );
+
+  if (!recipe) {
+    return <p>Recipe not found!</p>;
+  }
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipe.id} />
+    </div>
+  );
+};
+
+export default RecipeDetails;
