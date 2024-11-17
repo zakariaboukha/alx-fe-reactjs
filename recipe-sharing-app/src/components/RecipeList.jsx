c559ad90f2b10aaff046b196232e184dc74d5c5f
zakariaboukha <zakariaboy99@gmail.com>
2024-11-17 17:57:51 +0100

    Create RecipeList.jsx

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,22 @@
+import useRecipeStore from '../recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      {recipes.length === 0 ? (
+        <p>No recipes yet! Add one below.</p>
+      ) : (
+        recipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default RecipeList;
