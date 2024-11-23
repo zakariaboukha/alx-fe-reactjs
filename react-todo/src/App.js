083898121844bf908e6f406c8ee0558377b13e1d
zakariaboukha <zakariaboy99@gmail.com>
2024-11-23 20:13:06 +0100

    Create App.js

---
+++ react-todo/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import TodoList from './TodoList';  // Import TodoList component
+
+const App = () => {
+  return (
+    <div>
+      <TodoList />  {/* Render TodoList component */}
+    </div>
+  );
+};
+
+export default App;
