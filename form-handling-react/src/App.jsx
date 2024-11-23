bb32263a56f98f8116755df2b0d0d9779e717970
zakariaboukha <zakariaboy99@gmail.com>
2024-11-23 17:07:07 +0100

    Create App.jsx

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RegistrationForm from "./components/RegistrationForm";
+
+function App() {
+    return (
+        <div>
+            <h1>User Registration (Controlled Components)</h1>
+            <RegistrationForm />
+        </div>
+    );
+}
+
+export default App;
