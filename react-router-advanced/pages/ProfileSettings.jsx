e0877f744709caa9b1a2c82428b145392ac0b30d
zakariaboukha <zakariaboy99@gmail.com>
2024-11-23 19:16:28 +0100

    Delete react-router-advanced/pages directory

--- react-router-advanced/pages/ProfileSettings.jsx
+++
@@ -1,9 +0,0 @@
-// src/pages/ProfileSettings.jsx
-const ProfileSettings = () => (
-  <div>
-    <h2>Profile Settings</h2>
-    <p>Here, users can update their profile settings and preferences.</p>
-  </div>
-);
-
-export default ProfileSettings;
