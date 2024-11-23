115f2f4da7bba6896b282b5f6599811305aadf48
zakariaboukha <zakariaboy99@gmail.com>
2024-11-23 19:23:26 +0100

    Create ProfileSettings.jsx

---
+++ react-router-advanced/src/pages/ProfileSettings.jsx
@@ -0,0 +1,9 @@
+// src/pages/ProfileSettings.jsx
+const ProfileSettings = () => (
+  <div>
+    <h2>Profile Settings</h2>
+    <p>Here, users can update their profile settings and preferences.</p>
+  </div>
+);
+
+export default ProfileSettings;
