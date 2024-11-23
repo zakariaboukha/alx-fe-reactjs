f3368f238c029ba7caa4e43847be67a0c83a3637
zakariaboukha <zakariaboy99@gmail.com>
2024-11-23 19:16:13 +0100

    Delete react-router-advanced/components directory

--- react-router-advanced/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/ProtectedRoute.jsx
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children, isAuthenticated }) => {
-  return isAuthenticated ? children : <Navigate to="/" />;
-};
-
-export default ProtectedRoute;
