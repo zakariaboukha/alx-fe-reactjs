aad51d71ac436edb5d8a71b327a5ac18f7c509b4
zakariaboukha <zakariaboy99@gmail.com>
2024-11-23 19:14:16 +0100

    Create ProfileDetails.jsx

---
+++ react-router-advanced/pages/ProfileDetails.jsx
@@ -0,0 +1,9 @@
+// src/pages/ProfileDetails.jsx
+const ProfileDetails = () => (
+  <div>
+    <h2>Profile Details</h2>
+    <p>This section contains detailed information about the user profile.</p>
+  </div>
+);
+
+export default ProfileDetails;
