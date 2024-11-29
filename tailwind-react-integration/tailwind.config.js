d1b3d36c51d3b3126596a3ece04b636bae247abb
zakariaboukha <zakariaboy99@gmail.com>
2024-11-29 19:23:35 +0100

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"], // Specify paths to all templates
+  darkMode: "media", // Use 'media' for system dark mode, or 'class' for manual toggle
   theme: {
     extend: {}, // Extend Tailwind's default theme here
   },
