5334d17ab8680d0cce3dd6d717bf5679c61ec08d
zakariaboukha <zakariaboy99@gmail.com>
2024-12-15 17:58:52 +0100

    Add files via upload

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <p>Start building the application here.</p>
+    </div>
+  );
+};
+
+export default App;
