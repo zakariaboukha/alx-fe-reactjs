70a1e8aea92b082b481bf0d91cd598ac791d1e3c
zakariaboukha <zakariaboy99@gmail.com>
2024-12-15 18:10:48 +0100

    Create githubService.js

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.github.com/users';
+
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/${username}`);
+    return response.data;
+  } catch (error) {
+    throw new Error('User not found');
+  }
+};
